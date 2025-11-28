4772c3df085e533122633209077f0cc8234100fd
artyomdev250 <artyom.dev250@gmail.com>
2025-11-28 22:13:23 +0500

    branch setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
 
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </div>
   )
 }
