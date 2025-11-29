8f1943d5f438d3b23c2d114858dcb6184800b1d9
artyomdev250 <artyom.dev250@gmail.com>
2025-11-29 23:08:58 +0500

    form setup

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,6 +1,18 @@
 function SignInForm() {
     return (
-        <div>SignInForm</div>
+        <form className="w-fit">
+            <p className="font-bold text-[30px]">Welcome Back 👋</p>
+            <p className="mt-7 mb-12 text-[19px] max-w-[410px] text-[#313957]">Today is a new day. It's your day. You shape it. Sign in to start managing your items!</p>
+            <div className="flex flex-col gap-2 mb-6">
+                <span className="font-medium">Email</span>
+                <input type="email" className="input" placeholder="Example@email.com" required />
+            </div>
+            <div className="flex flex-col gap-2 mb-6">
+                <span className="font-medium">Password</span>
+                <input type="password" className="input" placeholder="At least 8 characters" required />
+            </div>
+            <button className="button">Sign in</button>
+        </form>
     )
 }
 
